d95b86869f4a9a3b1270341811ccceb43b329f4f
Advait Suresh <advaitsuresh2002@gmail.com>
2023-05-16 10:37:14 +0530

    add sign up form validation

--- chat/views.py
+++ chat/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+username = "nil"
+password = "nil"
 
 
 def index(request):
@@ -13,4 +15,9 @@ def login(request):
 
 
 def signup(request):
+    username = request.POST.get("fname")
+    password = request.POST.get("password")
+
+    print(f"User: {username} and Pass: {password}")
+
     return render(request, "signup.html")
