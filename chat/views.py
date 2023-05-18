fed186630f4efc180473e5a010edfed0bad4aa78
Advait Suresh <advaitsuresh2002@gmail.com>
2023-05-18 18:02:59 +0530

    login bug fixed

--- chat/views.py
+++ chat/views.py
@@ -5,10 +5,6 @@ from chat.models import User, Room, Message
 import random
 
 
-# Create your views here.
-curr_user = "adv#123"
-
-
 def generate_code():
     number = random.randint(1000, 9999)
     return number
@@ -19,46 +15,46 @@ def index(request):
 
 
 def login(request):
+    err = ""
+
     if request.method == "POST":
         username = request.POST.get("username")
         password = request.POST.get("password")
 
-        user = User.objects.get(username=username, password=password)
+        user = User.objects.filter(username=username, password=password)
 
-        curr_user = user.username
+        if user:
+            curr_user = user
+            return redirect("/home?username=" + username)
 
-    return render(request, "login.html")
+        else:
+            user = User.objects.filter(username=username)
+            if user:
+                err = "Password doesnt match"
+            else:
+                err = "User does not exist"
+
+    return render(request, "login.html", {"error": err})
 
 
 def signup(request):
     if request.method == "POST":
         fname = request.POST.get("fname")
         password = request.POST.get("password")
-        username = f"{fname}#{generate_code()}"
+        username = f"{fname}_{generate_code()}"
 
         user = User(fname=fname, username=username, password=password)
         user.save()
 
         print(f"fname: {fname} user:{username} pass: {password}")
 
-        return redirect("/demo-chat")
+        return redirect("/home?username=" + username)
 
     return render(request, "signup.html")
 
 
-def demo_chat(request):
-    u = User.objects.all()
-
-    user_dict = {
-        "username": curr_user,
-        "items": u,
-    }
-
-    return render(request, "demo-chat.html", user_dict)
-
-
 def home(request):
-    home_dict = {"username": curr_user}
+    home_dict = {"username": request.GET.get("username")}
 
     return render(request, "home.html", home_dict)
 
@@ -74,10 +70,8 @@ def room(request, room):
 
 
 def checkview(request):
-    print("hello")
-
     room = request.POST.get("room")
-    username = curr_user
+    username = request.POST.get("username")
 
     if Room.objects.filter(name=room).exists():
         return redirect("/" + room + "/?username=" + username)
@@ -91,12 +85,12 @@ def send(request):
     message = request.POST.get("message")
     room_id = request.POST.get("room_id")
     username = request.POST.get("username")
-    room = request.POST.get("room")
 
     new_message = Message.objects.create(value=message, user=username, room=room_id)
     new_message.save()
 
-    return redirect("/" + room + "/?username=" + username)
+    # return redirect("/" + room + "/?username=" + username)
+    return HttpResponse("Message sent successfully")
 
 
 def getmessages(request, room):
